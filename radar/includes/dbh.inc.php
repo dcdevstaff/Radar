991b9727ac1994e6611a72d12dfdc1e80f402bf8
dcdevstaff <dcdevstaff@gmail.com>
2018-03-27 17:46:50 +0200

    commentato file Db

--- radar/includes/dbh.inc.php
+++ radar/includes/dbh.inc.php
@@ -4,7 +4,7 @@ $dbServerName = "localhost";
 $dbUserName = "root";
 $dbPassword = "1234";
 $dbName = "sensordb";
-
+//cambiare pasw
 $conn = mysqli_connect($dbServerName,$dbUserName,$dbPassword,$dbName);
 
 ?>
